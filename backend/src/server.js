4f6abdbed50f03784e7ebbbd3b1b4ecf70669156
unknown <alvin.webdev.consular@gmail.com>
2025-10-21 17:49:22 +0800

    added/updated files for render deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,20 +8,27 @@ import notesRoutes from './routes/notesRoutes.js';
 import {connectDB} from './config/db.js';
 import rateLimiter from "./middleware/rateLimiter.js";
 
+import path from 'path';
+
 
 
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 
 
 // middleware to parse JSON request body
+if (process.env.NODE_ENV !== 'production') {
+    console.log("Development Mode: CORS enabled for http://localhost:5173");
 
-app.use(cors({
+    app.use(cors({
     origin: "http://localhost:5173",
     })
-);
+    );
+}
+
 
 app.use(express.json()); 
 app.use(rateLimiter);
@@ -33,7 +40,15 @@ app.use((req, res, next) => {
     next();
 })
 
-app.use("/api/notes", notesRoutes)
+app.use("/api/notes", notesRoutes);
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+    app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
+})
+}
 
 connectDB().then(() => {
     app.listen(5001, () => {
